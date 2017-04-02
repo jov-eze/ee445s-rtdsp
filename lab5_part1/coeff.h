bee9ef3cb2530e2b5a8ba99ecaedd5ee2e26238e
jov-eze <jcezeokafor@gmail.com>
2017-04-02 16:49:23 -0500

    lab 5 part 1

---
+++ lab5_part1/coeff.h
@@ -0,0 +1,11 @@
+// Welch, Wright, & Morrow, 
+// Real-time Digital Signal Processing, 2005
+
+/* coeff.h                             */
+/* FIR filter coefficients             */
+/* exported by MATLAB using FIR_DUMP2C */
+
+#define N 161
+
+extern float B[];
+
